1a300b4892aa7219ee0e1b30c995c4c9a88312b8
Aaron Westbrook <westbrook823@gmail.com>
2017-07-09 15:58:49 -0400

    Now with more styles

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <h1>Oh snap, we're Reacting!</h1>,
+  (
+    <div>
+      <h1>Oh snap, we're Reacting!</h1>
+      <h4>Now with styles!</h4>
+      <div className="flex-row">
+        {
+          [1, 2, 3, 4, 5].map((number) => <div className="demo-item">{number}</div>)
+        }
+      </div>
+    </div>
+  ),
   document.getElementById("app"),
 );
-
-const overengineeredHello = async () => {
-  const promise = new Promise((resolve) => {
-    setTimeout(() =>
-      resolve("Hey we can use cool stuff!"),
-    1000);
-  });
-  const message = await promise;
-  console.log(message);
-}
-
-overengineeredHello();
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const supportedBrowsers = require("./browsers");
+
+console.log(supportedBrowsers);
 
 const paths = {
   src: path.join(__dirname, "/src"),
   build: path.join(__dirname, "/build"),
 }
-
 const webpackConfig = {
   entry: [
     "babel-polyfill",
-    path.join(paths.src, "index.jsx")
+    path.join(paths.src, "index.jsx"),
+    path.join(paths.src, "scss/app.scss"),
   ],
   output: {
     path: paths.build,
     filename: "index.js",
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: "src/index.html",
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new ExtractTextPlugin("app.css"),
+  ],
   module: {
     rules: [
       {
@@ -26,21 +33,30 @@ const webpackConfig = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["react", ["env", {
-              targets: {
-                  browsers: [
-                    ">1%",
-                    "last 4 versions",
-                    "Firefox ESR",
-                    "not ie < 9",
-                  ],
+            presets: [
+              "react",
+              ["env", {
+                targets: {
+                  browsers: supportedBrowsers,
                 },
               }],
             ],
-          }
-        }
-      }
-    ]
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            "css-loader?importLoaders=1&minimize&sourceMaps&-autoprefixer",
+            "postcss-loader",
+            "sass-loader",
+          ],
+        }),
+      },
+
+    ],
   },
   devServer: {
     publicPath: "/",
